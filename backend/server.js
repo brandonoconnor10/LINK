0aeaf1e28107030f599f3b5deaaaf514031c1dd2
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2026-05-07 15:13:49 +0200

    chore: remove temporary test route after Atlas verification

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const linksRoutes = require('./routes/links');
+
 
 dotenv.config();
 connectDB();
@@ -9,7 +9,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(require('cors')());
-app.use('/api', linksRoutes);
+
 
 app.get('/', (req, res) => res.send('API running...'));
 
