c7517ce5007dbd90224f41ea2d8d1327d4f3e1be
Brandon O'Connor <brandonleeoconnor10@gmail.com>
2026-05-07 15:38:15 +0200

    Fixed server.js missing line not importing links.js router

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-
+const cors = require('cors');
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
+// Middleware
 app.use(express.json());
-app.use(require('cors')());
+app.use(cors());
 
+// Register Routes
+app.use('/api', require('./routes/links'));
 
-app.get('/', (req, res) => res.send('API running...'));
+app.get('/', (req, res) => {
+  res.send('API running...');
+});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
